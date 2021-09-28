4e6197c15a99c95b94d2d219b6d2da57f40526b6
Jakob Friedl <if20b089@technikum-wien.at>
2021-09-28 15:18:31 +0200

    Initial commit

---
+++ MTCG/MTCG/Cards/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTCG.Cards {
+    abstract class Card : ICard{
+        protected string Name { get; set; }
+	    protected readonly int Damage;
+	    protected ElementType ElementType { get; set; }
+
+	    protected Card(string name, int damage, ElementType type) {
+		    Name = name;
+		    Damage = damage;
+		    ElementType = type; 
+	    }
+
+    }
+}
