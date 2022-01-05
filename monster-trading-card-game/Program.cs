0d0ed5f9ceef704913246777ab89dddf535d66a3
your name <if20b089@technikum-wien.at>
2022-01-05 15:50:11 +0100

    Start Thread in Program.cs

--- monster-trading-card-game/Program.cs
+++ monster-trading-card-game/Program.cs
@@ -9,56 +9,62 @@ using Console = Colorful.Console;
 namespace monster_trading_card_game {
     class Program {
         static void Main(string[] args) {
+	        Thread thread = new Thread(StartGame); 
+			thread.Start();
+			thread.Join();
+        }
+
+        static void StartGame() {
 	        CLI cli = new CLI();
 
-	        Command cmd; 
-            while ((cmd = cli.GetUserCommand()) != Command.Quit) {
-				switch (cmd) {
-                    case Command.Register:
-	                    if (cli.RegisterUser()) {
-		                    Console.WriteLine("\nRegistration successful", Color.ForestGreen);
-							Thread.Sleep(1000);
-	                    } else {
-		                    Console.WriteLine("\nRegistration failed", Color.Red);
-							Thread.Sleep(1000);
-	                    }
-	                    break;
-                    case Command.Login:
-	                    if (cli.LoginUser()) {
-		                    Console.WriteLine("\nLogin successful", Color.ForestGreen);
-		                    Thread.Sleep(1000);
-	                    } else {
-		                    Console.WriteLine("\nLogin failed", Color.Red);
-		                    Thread.Sleep(1000);
-	                    }
-						break;
-                    case Command.Deck:
-                        cli.LoggedInUser.BuildDeck();
-	                    break;
-                    case Command.Trade:
-	                    cli.Trade(); 
-	                    break; 
-                    case Command.Battle:
-	                    cli.Battle();
-	                    break;
-                    case Command.Buy:
-	                    cli.GetPackage(); 
-	                    break;
-                    case Command.Scores:
-	                    cli.Scores(); 
-	                    break;
-					case Command.Profile:
-						cli.Profile();
-						break;
-                    case Command.Logout:
-	                    cli.LogoutUser();
-	                    break; 
-                    case Command.Quit:
-	                    break; 
-                    case Command.Invalid:
-	                    break;
-	            }
-            }
-        }
+	        Command cmd;
+	        while ((cmd = cli.GetUserCommand()) != Command.Quit) {
+		        switch (cmd) {
+			        case Command.Register:
+				        if (cli.RegisterUser()) {
+					        Console.WriteLine("\nRegistration successful", Color.ForestGreen);
+					        Thread.Sleep(1000);
+				        } else {
+					        Console.WriteLine("\nRegistration failed", Color.Red);
+					        Thread.Sleep(1000);
+				        }
+				        break;
+			        case Command.Login:
+				        if (cli.LoginUser()) {
+					        Console.WriteLine("\nLogin successful", Color.ForestGreen);
+					        Thread.Sleep(1000);
+				        } else {
+					        Console.WriteLine("\nLogin failed", Color.Red);
+					        Thread.Sleep(1000);
+				        }
+				        break;
+			        case Command.Deck:
+				        cli.LoggedInUser.BuildDeck();
+				        break;
+			        case Command.Trade:
+				        cli.Trade();
+				        break;
+			        case Command.Battle:
+				        cli.Battle();
+				        break;
+			        case Command.Buy:
+				        cli.GetPackage();
+				        break;
+			        case Command.Scores:
+				        cli.Scores();
+				        break;
+			        case Command.Profile:
+				        cli.Profile();
+				        break;
+			        case Command.Logout:
+				        cli.LogoutUser();
+				        break;
+			        case Command.Quit:
+				        break;
+			        case Command.Invalid:
+				        break;
+		        }
+	        }
+		}
     }
 }
